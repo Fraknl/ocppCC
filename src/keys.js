fb757bb8c577893ae3140022bcaa28379609f78e
Fraknl <frank.torres0410@ucuenca.edu.ec>
2022-02-12 19:15:58 -0500

    nuevas keys

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,8 @@
 module.exports = {
     database:{
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'sistema_central'
+        host: 'us-cdbr-east-05.cleardb.net',
+        user: 'b53c87d065b113',
+        password: '3b3e2cd6',
+        database: 'heroku_e468ad212f83084'
     }
 }
